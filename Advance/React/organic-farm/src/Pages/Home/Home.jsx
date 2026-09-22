a0574eafdf213b2713b8f074c24d58f5d6c9d4b4
mdadham <adhamnotheare@gmail.com>
2026-09-22 19:04:13 +0530

    Improved my Organic Website

--- Advance/React/organic-farm/src/Pages/Home/Home.jsx
+++ Advance/React/organic-farm/src/Pages/Home/Home.jsx
@@ -3,7 +3,7 @@ import "./Home.css"
 function home() {
     return (
         <div className="home">
-            <h1>ㅤ</h1>
+            {/* <h1>ㅤ</h1> */}
             <div className="container">
                 <h1 className="welcome-text">
                     <span className="bold-green">Organic</span> Foods at <br></br>your Doorsteps
